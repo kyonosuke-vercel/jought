b4486271ecf57dc6628286d08be7e7476cca36af
kyonosukewatanabe <kyowatan@usc.edu>
2025-08-21 22:14:05 -0700

    hello andre

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="font-sans min-h-screen flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col items-center gap-8">
         <Image src="/fox.png" alt="fox logo" width={180} height={38} priority />
-        <div className="text-center">Hi Swan.</div>
+        <div className="text-center">Hello Andrej Obradovic.</div>
         {/* <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Write anything that you want.
