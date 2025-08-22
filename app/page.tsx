fef04057e5298a1f49a0d384c5d0e748fe26532f
kyonosukewatanabe <kyowatan@usc.edu>
2025-08-21 19:46:58 -0700

    center

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/fox.png"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div>Hi Swan.</div>
+    <div className="font-sans min-h-screen flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col items-center gap-8">
+        <Image src="/fox.png" alt="fox logo" width={180} height={38} priority />
+        <div className="text-center">Hi Swan.</div>
         {/* <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Write anything that you want.
